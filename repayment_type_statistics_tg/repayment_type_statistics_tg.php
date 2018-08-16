17ed576bc4640934cb8cfd35d8375ba8c6193656
翟伟东 <891717760@qq.com>
2018-08-16 11:28:11 +0800

    输出忘记关了

--- repayment_type_statistics_tg/repayment_type_statistics_tg.php
+++ repayment_type_statistics_tg/repayment_type_statistics_tg.php
@@ -8,7 +8,6 @@
 	$now = date('Y-m-d',time());
 	$start = date('Y-m-d',(time()-7*24*60*60));
 	$query = "SELECT date,type,num,money FROM `repayment_type_statistics_tg` where date >=".'\''.$start.'\''." and date <=".'\''.$now.'\''." and type = '用户还款' order by date desc limit " .$index.','.$pageSize;//首次打开,显示30条
-	echo $query;
     $result = mysqli_query($link, $query);
 	$arr = $result->fetch_all(MYSQLI_ASSOC);
     //全部还款类型
